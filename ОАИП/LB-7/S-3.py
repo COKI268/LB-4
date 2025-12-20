90bd305eea0e18a1a35835598167e4ed5163afdd
COKI268 <azeno268zara.ru@gmail.com>
2025-12-20 15:22:33 +0500

    Delete ОАИП/LB-7 directory

--- ОАИП/LB-7/S-3.py
+++
@@ -1,9 +0,0 @@
-text = input("Введите текст")
-word = input("Введите слово для поиска")
-if word in text:
-    с = text.count(word)
-    print(f"Слово '{word}' найдено в тексте")
-    print(f"Количество: '{с}' ")
-else:
-    print(f"Слова '{word}' нет в тексте")
-    
--- ОАИП/LB-7/S-4.py
+++
@@ -1,6 +0,0 @@
-Goda = int(input("Введите год"))
-if Goda % 4 == 0 and Goda % 100 == 0 and Goda % 400 == 0:
-    print("Високосный год")
-else:
-    print("Не високосный год")
-   
