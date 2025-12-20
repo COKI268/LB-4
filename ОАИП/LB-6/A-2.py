57b217dcb36ad976ba33d49b7d64f976b4cbc2f0
COKI268 <azeno268zara.ru@gmail.com>
2025-12-20 15:22:26 +0500

    Delete ОАИП/LB-6 directory

--- ОАИП/LB-6/A-2.py
+++
@@ -1,8 +0,0 @@
-text = input("введите текст")
-f = input("введите что заменить и на что через пробел")
-s = f.split()
-str1 = s[0]
-str2 = s[1]
-Rtext = text.replace(str1,str2)
-print("результат")
-print(Rtext)
--- ОАИП/LB-6/A-3.py
+++
@@ -1,5 +0,0 @@
-word = input('введите текст')
-num = int(input("введите число:"))
-
-print(word[num])
-
